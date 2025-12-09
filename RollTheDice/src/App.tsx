d12092a1bfc8ba6d771913035a4320d800fb9f91
Merve Nur İregöz <20212022035@std.nisantasi.edu.tr.com>
2025-12-09 22:52:13 +0300

    haptic feedbacks added

--- RollTheDice/src/App.tsx
+++ RollTheDice/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import { StyleSheet, View, Text, ImageSourcePropType, Image, Pressable } from 'react-native';
 import { JSX } from 'react/jsx-dev-runtime';
+import RNHapticFeedback, { HapticOptions } from "react-native-haptic-feedback";
+
 
 import DiceOne from '../assets/dice-six-faces-one.png';
 //diceona hata vermesin diye srcde index.d.ts dosyasını kurup png modulu tanimladik.
@@ -11,6 +13,12 @@ import DiceFour from '../assets/dice-six-faces-four.png';
 import DiceFive from '../assets/dice-six-faces-five.png';
 import DiceSix from '../assets/dice-six-faces-six.png';
 
+const options: HapticOptions = {
+    enableVibrateFallback: true,
+    ignoreAndroidSystemSettings: false,
+  }
+  
+//
 type DiceProps = PropsWithChildren<{
   imageUrl: ImageSourcePropType
 }>
@@ -25,12 +33,19 @@ const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
     </View>
   )
 }
+//
+
+
+
 
 function App(): JSX.Element {
   const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
 
+  
+
   const rollDiceOnTop = () => {
     let randomNumber = Math.floor(Math.random() * 6) + 1;
+    RNHapticFeedback.trigger("selection", options);
 
     switch (randomNumber) {
       case 1:
@@ -56,14 +71,17 @@ function App(): JSX.Element {
         setDiceImage(DiceOne);
         break;
     }
+
+
+
   }
 
   return (
     <View style={styles.container}>
       <Dice imageUrl={diceImage} />
-      <Pressable onPress={rollDiceOnTop} style={styles.rollBtn}> 
-        <Text>Roll The Dice</Text> 
-        </Pressable>
+      <Pressable onPress={rollDiceOnTop} style={styles.rollBtn}>
+        <Text>Roll The Dice</Text>
+      </Pressable>
     </View>
   );
 }
@@ -75,15 +93,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  rollBtn:{
-    padding:10,
-    borderRadius:5,
-    borderColor:'#000',
-    borderWidth:1,
-    marginTop:20,
-    backgroundColor:'#f7c9f4ff',
-    opacity:0.8,
-    borderTopRightRadius:15
+  rollBtn: {
+    padding: 10,
+    borderRadius: 5,
+    borderColor: '#000',
+    borderWidth: 1,
+    marginTop: 20,
+    backgroundColor: '#f7c9f4ff',
+    opacity: 0.8,
+    borderTopRightRadius: 15
   }
 });
 
